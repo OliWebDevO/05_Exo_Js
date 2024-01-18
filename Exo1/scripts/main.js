a7da69f0a0f5f23df9661b0e25b5191b79151258
OliWebDevO <oliver-vdb@hotmail.com>
2024-01-18 14:27:34 +0100

    first exo

---
+++ Exo1/scripts/main.js
@@ -0,0 +1,42 @@
+// let utilisateurs = {
+//     user1: {
+//         nom: "Martin",
+//         age: 28,
+//         profession: "Ingénieur Logiciel",
+//         compétences: ["Java", "Python", "Docker"],
+//         adresse: {
+//             rue: "10 Rue de l'Innovation",
+//             ville: "Lyon",
+//             codePostal: "69000"
+//         }
+//     },
+//     user2: {
+//         nom: "Durand",
+//         age: 35,
+//         profession: "Designer Graphique",
+//         compétences: ["Photoshop", "Illustrator", "InDesign"],
+//         adresse: {
+//             rue: "55 Avenue de la Créativité",
+//             ville: "Marseille",
+//             codePostal: "13000"
+//         }
+//     },
+//     user3: {
+//         nom: "Lefebvre",
+//         age: 40,
+//         profession: "Analyste de Données",
+//         compétences: ["SQL", "R", "Tableau"],
+//         adresse: {
+//             rue: "20 Rue des Données",
+//             ville: "Nantes",
+//             codePostal: "44000"
+//         }
+//     }
+// };
+
+
+let texte = "hello world"
+
+console.log(texte);
+
+document.getElementById("p").innerHTML = texte;
